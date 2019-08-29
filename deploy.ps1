d50f6f589eafc97d8f2466d171ead0b663a43b12
Jalal Hejazi <jalal.hejazi@superusers.dk>
2019-08-29 15:42:46 +0200

    1.0.41 Fix: deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -2,10 +2,14 @@
 # semantic git deploy 
 param( $message )
 
-$package_version = node -p "require('./package.json').version"
-Write-Host  "Releasing Version: $package_version" -ForegroundColor Green
-
 git add .
-git commit -am "$package_version $message"
+
 npm version patch 
+
+$package_version = node -p "require('./package.json').version"
+
+git commit -am "$package_version $message"
+
 git push origin master --force --follow-tags 
+
+Write-Host  "Releasing Version: $package_version" -ForegroundColor Green
