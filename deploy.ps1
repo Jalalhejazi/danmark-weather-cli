3a83028282cca02366182c65511c433ce9d36244
Jalal Hejazi <jalal.hejazi@superusers.dk>
2019-08-29 15:44:56 +0200

    Fix: deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -4,12 +4,11 @@ param( $message )
 
 git add .
 
+git commit -am "$message"
+
 npm version patch 
-
-$package_version = node -p "require('./package.json').version"
-
-git commit -am "$package_version $message"
 
 git push origin master --force --follow-tags 
 
+$package_version = node -p "require('./package.json').version"
 Write-Host  "Releasing Version: $package_version" -ForegroundColor Green
