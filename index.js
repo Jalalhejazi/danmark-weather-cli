104e61f0f12436d39070218185b429487485326a
Jalal Hejazi <jalal.hejazi@gmail.com>
2019-08-29 16:40:19 +0200

    Fix

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const colors = require('colors')
 const config = require('./package')
 
-exports.printMsg = function(color='red', msg) {
-  console.log(config.version, msg[color])
-}
+exports.printMsg = (color='red', msg) => console.log(config.version, msg[color])
+
