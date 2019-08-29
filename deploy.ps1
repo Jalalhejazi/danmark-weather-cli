e9216a5d775ede1f1f44d2f8fba12eef90dcfd79
Jalal Hejazi <jalal.hejazi@superusers.dk>
2019-08-29 15:45:57 +0200

    Fix: deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,6 @@
 # Powershell 
 # semantic git deploy 
-param( $message )
+param( $message = "Fix" )
 
 git add .
 
@@ -8,7 +8,7 @@ git commit -am "$message"
 
 npm version patch 
 
-git push origin master --force --follow-tags 
+git push origin master --force --follow-tags --quite
 
 $package_version = node -p "require('./package.json').version"
 Write-Host  "Releasing Version: $package_version" -ForegroundColor Green
